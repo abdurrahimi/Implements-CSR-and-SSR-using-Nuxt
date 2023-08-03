310590ca20826877a478c297b4aeb12e99090464
Abdur Rahimi <abdur.rahimi@uangme.com>
2023-08-03 13:36:28 +0700

    init project

---
+++ pages/csr.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <!-- {{ entry.data }} -->
+        <table>
+            <thead>
+                <tr>
+                    <th>Nation</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="item in entry.data">
+                    <td>{{ item.Nation }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'IndexPage',
+
+    data() {
+        return {
+            entry: []
+        }
+    },
+    mounted() {
+        this.getData()
+    },
+    methods: {
+        async getData() {
+            try {
+                const response = await this.$axios.get('https://api.datausa.io/api/data?drilldowns=Nation&measures=Population')
+                this.entry = response.data
+            } catch (error) {
+                console.error("failed")
+            }
+        }
+    }
+}
+</script>
+  
---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <!-- {{ entry.data }} -->
+    <table>
+      <thead>
+        <tr>
+          <th>Nation</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in entry.data">
+          <td>{{item.Nation}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  async asyncData({ $axios }) {
+    try {
+      const response = await $axios.get('https://api.datausa.io/api/data?drilldowns=Nation&measures=Population')
+      //console.log(response.data)
+      return { entry: response.data }
+    } catch (error) {
+      //console.error('Error fetching data:', error.message)
+      return { entry: null }
+    }
+  },
+  data(){
+    return{
+      entry:[]
+    }
+  },
+  mounted(){
+    this.getData()
+  },
+  methods: {
+    async getData() {
+      
+    }
+  }
+}
+</script>
